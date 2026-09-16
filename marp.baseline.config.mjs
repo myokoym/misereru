5d607957d129e0fd4b3eba789da78489d8bd4591
Masafumi Yokoyama <myokoym@gmail.com>
2026-09-16 20:47:02 +0900

    prototype: add baseline Marp config

---
+++ marp.baseline.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from '@marp-team/marp-cli'
+
+export default defineConfig({
+  lang: 'ja',
+  themeSet: ['./themes/misereru-baseline.css'],
+  theme: 'misereru-baseline',
+})
